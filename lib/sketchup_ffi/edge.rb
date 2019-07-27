ada16ffb7f9c8262c6edf03ef58ac051d7819fdd
Jim Foltz <jim.foltz@gmail.com>
2019-07-27 16:04:37 -0400

    edge, face, and guide point functions

--- lib/sketchup_ffi/edge.rb
+++ lib/sketchup_ffi/edge.rb
@@ -1,8 +1,23 @@
+# edge.rb
 module SketchupFFI
 
-  attach_function(:SUEdgeCreate, [SUEdgeRef, SUPoint3d, SUPoint3d], SUResult)
+  attach_function(:SUEdgeToEntity, [SUEdgeRef], SUEntityRef)
+  attach_function(:SUEdgeFromEntity, [SUEntityRef], SUEdgeRef)
   attach_function(:SUEdgeToDrawingElement, [SUEdgeRef], SUDrawingElementRef)
+  attach_function(:SUEdgeFromDrawingElement, [SUDrawingElementRef], SUEdgeRef)
+  attach_function(:SUEdgeCreate, [SUEdgeRef, SUPoint3d, SUPoint3d], SUResult)
+  attach_function(:SUEdgeRelease, [SUEdgeRef], SUResult)
+  attach_function(:SUEdgeGetCurve, [SUEdgeRef, SUCurveRef], SUResult)
   attach_function(:SUEdgeGetStartVertex, [SUEdgeRef, SUVertexRef], SUResult)
+  attach_function(:SUEdgeGetEndVertex, [SUEdgeRef, SUVertexRef], SUResult)
+  attach_function(:SUEdgeSetSoft, [SUEdgeRef, :bool], SUResult)
+  attach_function(:SUEdgeGetSoft, [SUEdgeRef, :bool_ptr], SUResult)
+  attach_function(:SUEdgeSetSmooth, [SUEdgeRef, :bool], SUResult)
+  attach_function(:SUEdgeGetSmooth, [SUEdgeRef, :bool_ptr], SUResult)
+  attach_function(:SUEdgeGetNumFaces, [SUEdgeRef, :size_ptr], SUResult)
+  attach_function(:SUEdgeGetFaces, [SUEdgeRef, :size_ptr, SUFaceRef, :size_ptr], SUResult)
+  attach_function(:SUEdgeGetColor, [SUEdgeRef, SUColor], SUResult)
   attach_function(:SUEdgeSetColor, [SUEdgeRef, SUColor], SUResult)
+  attach_function(:SUEdgeGetLengthWithTransform, [SUEdgeRef, SUTransformation, :double_ptr], SUResult)
 
 end
--- lib/sketchup_ffi/guide_point.rb
+++ lib/sketchup_ffi/guide_point.rb
@@ -1,6 +1,14 @@
 # guide-point.rb
 module SketchupFFI
 
-  attach_function(:SUGuidePointCreate, [SUGuidePointRef, :pointer], SUResult)
+  attach_function(:SUGuidePointToEntity, [SUGuidePointRef], SUEntityRef)
+  attach_function(:SUGuidePointFromEntity, [SUEntityRef], SUGuidePointRef)
+  attach_function(:SUGuidePointToDrawingElement, [SUGuidePointRef], SUDrawingElementRef)
+  attach_function(:SUGuidePointFromDrawingElement, [SUDrawingElementRef], SUGuidePointRef)
+  attach_function(:SUGuidePointCreate, [SUGuidePointRef, SUPoint3d], SUResult)
+  attach_function(:SUGuidePointRelease, [SUGuidePointRef], SUResult)
+  attach_function(:SUGuidePointGetPosition, [SUGuidePointRef, SUPoint3d], SUResult)
+  attach_function(:SUGuidePointGetFromPosition, [SUGuidePointRef, SUPoint3d], SUResult)
+  attach_function(:SUGuidePointGetDisplayAsLine, [SUGuidePointRef, :bool_ptr], SUResult)
 
 end
