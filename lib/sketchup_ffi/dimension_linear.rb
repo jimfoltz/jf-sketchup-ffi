611b32983506a0e8083ecebe82698fce5fb38887
Jim Foltz <jim.foltz@gmail.com>
2019-07-30 19:19:30 -0400

    add dimension functions

---
+++ lib/sketchup_ffi/dimension_linear.rb
@@ -0,0 +1,44 @@
+require "sketchup_ffi/geometry"
+
+module SketchupFFI
+
+  SUHorizontalTextPositionType = enum(
+    :Center,
+    :OutsideStart,
+    :OutsideEnd
+  )
+
+  SUVerticalTextPositionType = enum(
+    :Center,
+    :Above,
+    :Below
+  )
+
+  SUDimensionLinearAlignmentType = enum(
+    :Aligned,
+    :Vertical,
+    :Horizontal
+  )
+
+  attach_function(:SUDimensionLinearToDimension, [SUDimensionLinearRef], SUDimensionRef)
+  attach_function(:SUDimensionLinearFromDimension, [SUDimensionRef], SUDimensionLinearRef)
+  attach_function(:SUDimensionLinearCreate, [SUDimensionLinearRef, SUPoint3d, SUInstancePathRef, SUPoint3d, SUInstancePathRef, :double], SUResult)
+  attach_function(:SUDimensionLinearRelease, [SUDimensionLinearRef], SUResult)
+  attach_function(:SUDimensionLinearGetStartPoint, [SUDimensionLinearRef, SUPoint3d, SUInstancePathRef], SUResult)
+  attach_function(:SUDimensionLinearSetStartPoint, [SUDimensionLinearRef, SUPoint3d, SUInstancePathRef], SUResult)
+  attach_function(:SUDimensionLinearGetEndPoint, [SUDimensionLinearRef, SUPoint3d, SUInstancePathRef], SUResult)
+  attach_function(:SUDimensionLinearSetEndPoint, [SUDimensionLinearRef, SUPoint3d, SUInstancePathRef], SUResult)
+  attach_function(:SUDimensionLinearGetXAxis, [SUDimensionLinearRef, SUVector3d], SUResult)
+  attach_function(:SUDimensionLinearSetXAxis, [SUDimensionLinearRef, SUVector3d], SUResult)
+  attach_function(:SUDimensionLinearGetNormal, [SUDimensionLinearRef, SUVector3d], SUResult)
+  attach_function(:SUDimensionLinearSetNormal, [SUDimensionLinearRef, SUVector3d], SUResult)
+  attach_function(:SUDimensionLinearGetPosition, [SUDimensionLinearRef, SUPoint2d], SUResult)
+  attach_function(:SUDimensionLinearSetPosition, [SUDimensionLinearRef, SUPoint2d], SUResult)
+  attach_function(:SUDimensionLinearGetVerticalAlignment, [SUDimensionLinearRef, SUVerticalTextPositionType], SUResult)
+  attach_function(:SUDimensionLinearSetVerticalAlignment, [SUDimensionLinearRef, SUVerticalTextPositionType], SUResult)
+  attach_function(:SUDimensionLinearGetHorizontalAlignment, [SUDimensionLinearRef, SUHorizontalTextPositionType], SUResult)
+  attach_function(:SUDimensionLinearSetHorizontalAlignment, [SUDimensionLinearRef, SUHorizontalTextPositionType], SUResult)
+  attach_function(:SUDimensionLinearGetAlignment, [SUDimensionLinearRef, SUDimensionLinearAlignmentType], SUResult)
+  attach_function(:SUDimensionLinearGetTextPosition, [SUDimensionLinearRef, SUPoint3d], SUResult)
+
+end
---
+++ lib/sketchup_ffi/dimension_radial.rb
@@ -0,0 +1,15 @@
+module SketchupFFI
+
+  attach_function(:SUDimensionRadialToDimension, [SUDimensionRadialRef], SUDimensionRef)
+  attach_function(:SUDimensionRadialFromDimension, [SUDimensionRef], SUDimensionRadialRef)
+  attach_function(:SUDimensionRadialCreate, [SUDimensionRadialRef, SUInstancePathRef], SUResult)
+  attach_function(:SUDimensionRadialRelease, [SUDimensionRadialRef], SUResult)
+  attach_function(:SUDimensionRadialGetCurveInstancePath, [SUDimensionRadialRef, SUInstancePathRef], SUResult)
+  attach_function(:SUDimensionRadialSetCurveInstancePath, [SUDimensionRadialRef, SUInstancePathRef], SUResult)
+  attach_function(:SUDimensionRadialGetDiameter, [SUDimensionRadialRef, :bool_ptr], SUResult)
+  attach_function(:SUDimensionRadialSetDiameter, [SUDimensionRadialRef, :bool], SUResult)
+  attach_function(:SUDimensionRadialGetLeaderBreakPoint, [SUDimensionRadialRef, SUPoint3d], SUResult)
+  attach_function(:SUDimensionRadialSetLeaderBreakPoint, [SUDimensionRadialRef, SUPoint3d], SUResult)
+  attach_function(:SUDimensionRadialGetLeaderPoints, [SUDimensionRadialRef, SUPoint3d], SUResult)
+
+end
