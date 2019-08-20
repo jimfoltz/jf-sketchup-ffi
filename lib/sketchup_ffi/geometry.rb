5ddbfc37a1303bdfda46092ec96a09217c43af6f
Jim Foltz <jim.foltz@gmail.com>
2019-08-20 17:27:18 -0400

    change default vector to (0, 0, 1)

--- lib/sketchup_ffi/geometry.rb
+++ lib/sketchup_ffi/geometry.rb
@@ -21,7 +21,7 @@ module SketchupFFI
       :y, :double,
       :z, :double
     )
-    def self.create(x = 0, y = 0, z = 0)
+    def self.create(x = 0, y = 0, z = 1)
       vector = new
       vector[:x] = x
       vector[:y] = y
