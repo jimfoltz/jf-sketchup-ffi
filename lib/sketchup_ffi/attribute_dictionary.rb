c6b66a2c132108affa412b882ec7d25dc9784ad9
Jim Foltz <jim.foltz@gmail.com>
2019-08-20 17:22:51 -0400

    added helper method to add attrib

--- lib/sketchup_ffi/attribute_dictionary.rb
+++ lib/sketchup_ffi/attribute_dictionary.rb
@@ -1,6 +1,8 @@
 # attribute-dictionary.rb
 #
+
 module SketchupFFI
+
   attach_function(:SUAttributeDictionaryCreate, [SUAttributeDictionaryRef, :string], SUResult)
   attach_function(:SUAttributeDictionaryRelease, [SUAttributeDictionaryRef], SUResult)
   attach_function(:SUAttributeDictionaryToEntity, [SUAttributeDictionaryRef], SUEntityRef)
@@ -17,9 +19,7 @@ module SketchupFFI
     attribute_dictionary_get_num_keys dict[:ptr], n
     n = n.read_int
     ptr = FFI::MemoryPointer.new SUStringRef, n
-    strs = n.times.collect {|i|
-      SUStringRef.new(ptr + i * SUStringRef.size)
-    }
+    strs = n.times.collect {|i| SUStringRef.new(ptr + i * SUStringRef.size) }
     strs.each_with_index {|s, i| string_create strs[i]}
     ret = FFI::MemoryPointer.new :size_t
     attribute_dictionary_get_keys dict[:ptr], n, ptr, ret
@@ -29,5 +29,11 @@ module SketchupFFI
     keys
   end
 
-end
+  def self.attr_dict_set_value(dict, key, value)
+    typed_value = value_to_typed_value(value)
+    attribute_dictionary_set_value(dict[:ptr], key, typed_value)
+    #typed_value_release typed_value
+  end
+
+end # module SketchupFFI
 
